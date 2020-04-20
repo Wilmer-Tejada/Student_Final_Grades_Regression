872599c8e9cd44ba496facc94df8a6806858de44
Wilmer Tejada <wilmer.tejada@yahoo.com>
2020-04-20 10:38:57 -0700

    Add files via upload

---
+++ Linear Regression with Scatter Plot.py
@@ -0,0 +1,59 @@
+import tensorflow as tf
+print(tf.__version__)
+import keras
+import numpy as np
+import pandas as pd
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
+
+
+data = pd.read_csv("data/student-mat.csv", sep = ";")
+
+data = data[["G1","G2","G3","studytime","failures","absences"]]
+
+predict = "G3"
+
+x = np.array(data.drop([predict],1))
+y = np.array(data[predict])
+
+x_train,x_test,y_train,y_test = sklearn.model_selection.train_test_split(x,y, test_size=.1)
+
+best = 0
+for i in range(30):
+
+    x_train,x_test,y_train,y_test = sklearn.model_selection.train_test_split(x,y, test_size=.1)
+
+    linear = linear_model.LinearRegression()
+    linear.fit(x_train,y_train)
+    acc = linear.score(x_test,y_test)
+    print(acc)
+
+    if acc > best:
+        best = acc
+    # Saves model using the pickle module. The reason you want to save a model is because training a model everytime takes a lot of time. Some models take a long time to train so its better to save the model.
+        with open("studentmodel.pickle","wb") as f:
+        pickle.dump(linear, f)
+
+pickle_in = open("studentmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
+
+print('Coefficient:', linear.coef_)
+print('Intercept:', linear.intercept_)
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):
+    print(predictions[x],x_test[x], y_test[x])
+
+p = 'G1'
+style.use('ggplot')
+
+pyplot.scatter(data[p],data['G3'])
+pyplot.ylabel('Final Grade')
+pyplot.xlabel('G3')
+pyplot.show()
+
